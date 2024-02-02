b90b706bae2391e89cb0b0a5cdfc60b91e8b3fc8
JPEG <139462407+jpeg-artifact@users.noreply.github.com>
2024-02-02 11:55:59 +0100

    feat: added Properties to Book class

--- Library/Library/Book.cs
+++ Library/Library/Book.cs
@@ -8,5 +8,22 @@ namespace Library
 {
     internal class Book
     {
+        public Book(string title, string author, Genre genre, string description, int pages, Tags tags) 
+        {
+            Title = title;
+            Author = author;
+            Genre = genre;
+            Description = description;
+            Pages = pages;
+            Tags = tags;
+        }
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public Genre Genre { get; set; }
+        public string Description { get; set; }
+        public int Pages { get; set; }
+        public Tags Tags { get; set; }
+    }
     }
 }
---
+++ Library/Library/Enums.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+enum Genre
+{
+    Bildningsroman,
+    Brevroman,
+    Dagboksroman,
+    Pikareskroman,
+    Äventyrsroman,
+    Kriminalroman,
+    Thriller,
+    Historiskroman,
+    Fabel,
+    Krönika,
+    Legend,
+    Miniatyr,
+    Myt,
+    Novell,
+    Novellett,
+    Parabel,
+    Saga,
+    Folksaga,
+    Konstsaga,
+    Skiss,
+    Alexandrin,
+    Canzon,
+    Distikon,
+    Elegi,
+    Haiku,
+    Sapfiskstrof,
+    Sonett,
+    Lustspel,
+    Satyrspel,
+    Musikal,
+    Operett
+}
+
+[Flags]
+enum Tags
+{
+    None = 0,
+    Barn = 1,
+    Ungdom = 2,
+    Vuxen = 4,
+}
