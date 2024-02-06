864c72a08fb372c5757af00f9753aba9a4257122
JPEG <139462407+jpeg-artifact@users.noreply.github.com>
2024-02-06 11:29:34 +0100

    feat: added add command

--- Library/Library/Program.cs
+++ Library/Library/Program.cs
@@ -4,17 +4,34 @@
     {
         static void Main(string[] args)
         {
+            string[] commandChunks;
             bool isRunning = true;
+            List<Book> books = new();
 
             while (isRunning)
             {
-
+                commandChunks = Console.ReadLine().Split(";");
+                switch (commandChunks[0].ToLower())
+                {
+                    case "add":
+                        Console.WriteLine(commandChunks[0].ToLower());
+                        AddBook(books, commandChunks); // add;Title;Author;Genre;Description;Pages;Tags
+                        break;
+                }
             }
         }
 
-        static void AddBook()
+        static void AddBook(List<Book> books, string[] commandChunks)
         {
+            string title = commandChunks[1];
+            string author = commandChunks[2];
+            Genre genre = Enum.Parse<Genre>(commandChunks[3]);
+            string description = commandChunks[4];
+            int pages = int.Parse(commandChunks[5]);
+            string[] tagsChunk = commandChunks[6].Split(",");
+            int tags = tagsChunk.Select(Enum.Parse<Tags>).Cast<int>().Sum();
 
+            books.Add(new Book(title, author, genre, description, pages, (Tags)tags));
         }
     }
 }
