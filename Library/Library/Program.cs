b9214f3698f82ccfeda48fea9c4e4a61c0deab49
JPEG <139462407+jpeg-artifact@users.noreply.github.com>
2024-02-09 11:56:54 +0100

    feat: read and write from file

--- Library/Library/Program.cs
+++ Library/Library/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            string password = new StreamReader("Password.txt").ReadLine();
             string[] commandChunks;
             bool isRunning = true;
             List<Book> books = new();
@@ -16,7 +17,8 @@
                 {
                     case "add":
                         Console.WriteLine(commandChunks[0].ToLower());
-                        AddBook(books, commandChunks); // add;Title;Author;Genre;Description;Pages;Tags
+                        AddBook(books, commandChunks, EnterPassword(password)); // add;Title;Author;Genre;Description;Pages;Tags
+                        WriteFile(books);
                         break;
                     case "print":
                         PrintAll(books);
@@ -25,8 +27,63 @@
             }
         }
 
-        static void AddBook(List<Book> books, string[] commandChunks)
+        static void WriteFile(List<Book> books)
         {
+            StreamWriter writeFile = new("LibraryData.txt");
+
+            foreach (Book book in books)
+            {
+                writeFile.WriteLine($"{book.Title};{book.Author};{book.Genre};{book.Description};{book.Pages};{book.Tags}");
+            }
+
+            writeFile.Close();
+        }
+
+        static void ReadFile()
+        {
+            StreamReader readFile = new("LibraryData.txt");
+            string s;
+
+            while ((s = readFile.ReadLine()) != null)
+            {
+
+            }
+        }
+
+        static bool EnterPassword(string password)
+        {
+            Console.Write("Enter password: ");
+            string enteredPassword = Console.ReadLine();
+            if (enteredPassword == password)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static Book StringToBook(string[] s)
+        {
+            string title = s[1];
+            string author = s[2];
+            Genre genre = Enum.Parse<Genre>(s[3]);
+            string description = s[4];
+            int pages = int.Parse(s[5]);
+            string[] tagsChunk = s[6].Split(",");
+            int tags = tagsChunk.Select(Enum.Parse<Tags>).Cast<int>().Sum();
+            return new Book(title, author, genre, description, pages, (Tags)tags);
+        }
+
+        static void AddBook(List<Book> books, string[] commandChunks, bool isPasswordCorrect)
+        {
+            if (!isPasswordCorrect)
+            {
+                Console.WriteLine("Incorrect password");
+                return;
+            }
+
             string title = commandChunks[1];
             string author = commandChunks[2];
             Genre genre = Enum.Parse<Genre>(commandChunks[3]);
